bfb22a68b564a6490059b8caf01b2a251e425c77
Philipp Stehle <philipp.stehle@sap.com>
2017-09-19 15:47:06 +0200

    Refactor Join-Dialog

---
+++ public/JoinDialog.js
@@ -0,0 +1,31 @@
+class JoinDialog {
+  constructor($scope) {
+    $scope.joinGroup = (group, password) => {
+      let promiseGroupId;
+      if(group == null){
+        promiseGroupId = getChannelByName($scope.searchText)
+          .then(res => res.channel.id);
+      }
+      else {
+        promiseGroupId = Promise.resolve(group.id);
+      }
+      promiseGroupId.then(groupId => subscribeToChannel(groupId, password))
+        .then($scope.hideDialog.bind($scope))
+        .then($scope.updateGroupsBinding.bind($scope))
+        .catch($scope.handleFetchError.bind($scope));
+    };
+  }  
+}
+
+function JoinDialogFactory($scope, $mdDialog, parent, ev) {
+  parent = parent instanceof Element ? angular.element(parent) : parent;
+  $mdDialog.show({
+    controller: JoinDialog,
+    templateUrl: 'JoinDialog.html',
+    parent: parent,
+    targetEvent: ev,
+    clickOutsideToClose: true,
+    preserveScope: true,
+    scope: $scope.$new()
+  });
+}
